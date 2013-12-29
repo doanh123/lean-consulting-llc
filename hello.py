531867d1237569710243ce0794f0bf775277a547
root <root@ip-172-31-17-5.us-west-2.compute.internal>
2013-12-29 11:04:05 +0000

    about.html

--- hello.py
+++ hello.py
@@ -7,4 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/home')
+def index():
+    return render_template('home.html')
+
+
+
+
