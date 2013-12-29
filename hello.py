3b72dc4e5ca0665da1ac840a352d3ce53e1eea2e
root <root@ip-172-31-17-5.us-west-2.compute.internal>
2013-12-29 13:57:58 +0000

    contact.html

--- hello.py
+++ hello.py
@@ -16,3 +16,12 @@ def index():
 @app.route('/aboutus')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/help')
+def contact():
+    return render_template('contact.html')
+
