7bb035199ed9d339398231dae40ea959f931bf47
root <root@ip-172-31-17-5.us-west-2.compute.internal>
2013-12-29 12:49:40 +0000

    hello.pu

--- hello.py
+++ hello.py
@@ -16,25 +16,3 @@ def index():
 @app.route('/aboutus')
 def index():
     return render_template('about.html')
-
-"""
-@app.route('/services')
-def index():
-    return render_template('services.html')
-
-@app.route('/leantraining')
-def index():
-   return render_template('leantraining.html')
-
-@app.route('/contact')
-def index():
-   return render_template('contact.html')
-
-@app.route('/help')
-def index():
-   return render_template('help.html')
-
-@app.route('/login')
-def index():
-   return render_template('login.html')
-"""
